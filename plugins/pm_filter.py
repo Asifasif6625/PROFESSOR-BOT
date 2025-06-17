f545802bc526c1630d79c893ffb1b6e56ef13c8a
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2025-06-18 02:41:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -209,9 +209,14 @@ async def pm_spoll_choker(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I Cᴏᴜʟᴅɴ'ᴛ Fɪɴᴅ Aɴʏ Mᴏᴠɪᴇ Iɴ Tʜᴀᴛ Nᴀᴍᴇ", quote=True)
-        await asyncio.sleep(10)
-        return await k.delete()
+        await msg.reply_photo(
+            photo="https://telegra.ph/file/your_image.png",  # 🔁 Replace with your image URL or file path
+            caption="❌ **Movie Not Found!**\n\nWe couldn't find that movie in our database. Please check the name and try again.",
+            parse_mode="markdown",
+            quote=True
+        )
+        await asyncio.sleep(20)
+        return
     regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
     gs = list(filter(regex.match, g_s))
     gs_parsed = [re.sub(r'\b(\-([a-zA-Z-\s])\-\simdb|(\-\s)?imdb|(\-\s)?wikipedia|\(|\)|\-|reviews|full|all|episode(s)?|film|movie|series)', '', i, flags=re.IGNORECASE) for i in gs]
